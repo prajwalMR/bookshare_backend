fd97fa9a5c7b8514e0feae22e16cbdc0f4310669
shashanktatti <tattishashank@gmail.com>
2017-03-30 23:42:51 +0530

    added email verification

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.post('/users/signup',auth.signup);
 router.post('/sendresetlink',auth.sendmail);
 router.post('/resetpsswd',auth.resetpass);
 router.post('/authenticate' , auth.authenticate);
+router.post('/verifyaccount',auth.verifyAccount);
 
 module.exports = router;
