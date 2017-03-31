fb0807f63efbe060f633537b02af54a95d75d7d5
shashanktatti <tattishashank@gmail.com>
2017-03-31 21:21:35 +0530

    user update details

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ var auth = require('./auth');
 
 router.post('/users/signin',auth.signin);
 router.post('/users/signup',auth.signup);
+router.post('/users/update',auth.UpdateDetails);
 router.post('/sendresetlink',auth.sendmail);
 router.post('/resetpsswd',auth.resetpass);
 router.post('/authenticate' , auth.authenticate);
 router.post('/verifyaccount',auth.verifyAccount);
-
+router.get('/getuserdetails',auth.getUserDetails);
 module.exports = router;
